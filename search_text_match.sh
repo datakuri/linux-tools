076cd1214e5d2c5a52bb60bfb77d6afbe3548f48
datakuri <pavanc.tech@outlook.com>
2020-07-18 14:45:41 -0400

    Cleanup unnecessary directories

--- search_text_match.sh
+++ search_text_match.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+function e_test {
+  if test $1 -gt 0
+  then
+    echo "failed"
+  elif [ $1 == 0 ]
+  then
+    echo "success"
+  fi
+}
+
 echo "hello just test"
-grep -ioH db.* testdir/*.sql
+grep -ioH db.* testdir/*.txt
+e_test $?
